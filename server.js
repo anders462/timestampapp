531271fa4cd1d512aa1c580b9a27ad8808cb98a4
anders462 <anders462@gmail.com>
2016-01-04 08:46:30 -0800

    added index.html

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req,res){
-  res.send("Hello Worlds");
+  res.send("index.html");
 });
 
 app.listen(app.get('port'), function(){
