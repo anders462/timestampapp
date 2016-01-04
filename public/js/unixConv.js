13eeff65b53ee325d1b5e95e1eb0634ec53612e3
anders462 <anders462@gmail.com>
2016-01-04 14:42:57 -0800

    working version

---
+++ public/js/unixConv.js
@@ -0,0 +1,25 @@
+var unixTime = require('unix-time');
+module.exports = function (req, callBack) {
+var monthMap = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+var q = req.slice(1).replace(/\%20/g," ");
+var time = {};
+var d;
+  if (!isNaN(q)) {
+    d = new Date(q*1000);
+    time = JSON.stringify({unix:q,natural:monthMap[d.getUTCMonth()] + " " + d.getUTCDate() + ", " + d.getUTCFullYear()});
+  } else {
+      d = new Date(q);
+      var year = d.getUTCFullYear();
+      var month = d.getUTCMonth();
+      var day = d.getUTCDate();
+      var uT = Date.UTC(year,month,day)/1000;
+      if ( !isNaN( d.getDate() ) ) {
+        time = JSON.stringify({unix: uT , natural: monthMap[month] + " " + day + ", " + year});
+      }  else {
+        time = {"unix": null, "natural": null };
+      }
+  }
+
+ return callBack(null, time);
+
+};
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 'use strict';
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 4000));
+var unixConv = require('./public/js/unixConv.js')
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req,res){
-  res.send("index.html");
+    res.send("index.html");
 });
 
+app.get('/*', function(req,res) {
+  unixConv(req.url, function(err,time){
+    if (!err){
+    res.send(time);
+  } else {
+    res.send("Error: " + err);
+  }
+  });
+
+});
+
+
+
 app.listen(app.get('port'), function(){
-  console.log("server is running on port 4000...");
+  console.log("server is running on port 5000...");
 });
