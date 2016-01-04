c9c96753556621762840941f4a6a2216bca5e54f
anders462 <anders462@gmail.com>
2016-01-04 09:54:16 -0800

    fixed port issue add css

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 'use strict';
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 4000));
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req,res){
   res.send("index.html");
 });
 
 app.listen(app.get('port'), function(){
-  console.log("server is running on port 5000...");
+  console.log("server is running on port 4000...");
 });
