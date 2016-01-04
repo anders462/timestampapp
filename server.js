c41842a28f099c9a05f7b04d93503d596f24dfdb
anders462 <anders462@gmail.com>
2016-01-03 16:27:13 -0800

    first build

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var app = express();
 
 app.get('/', function(req,res){
   res.send("Hello World");
-}).listen(3000);
+});
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000...");
+});
