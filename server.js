c4a2e655bdbde88367cd21046d0414532d88ed50
anders462 <anders462@gmail.com>
2016-01-03 16:57:52 -0800

    changed port conf in server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req,res){
   res.send("Hello World");
 });
 
-app.listen(5000, function(){
+app.listen(app.get('port'), function(){
   console.log("server is running on port 5000...");
 });
